0546b3d331cba14f34fdf54b36946cc4d4a68c04
michaelrmartin <iammichaelmartin@gmail.com>
2023-07-27 10:41:22 -0500

    Adds fetch_recipe action and logic to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,49 @@
 class RecipesController < ApplicationController
+
+  def fetch_recipe
+
+    url = params[:url]
+    html = URI.open(url)
+    doc = Nokogiri::HTML(html)
+    json = doc.css('script[type="application/ld+json"]')
+    parsed = JSON.parse(json.text)
+    
+    article = parsed_data["@graph"].find { |item| item["@type"] == "Article" }
+
+    if article
+      title = article["headline"]
+      chef = article["author"]["name"]
+      image = article["image"]["thumbnailUrl"]
+    
+      
+      recipe = parsed_data["@graph"].find { |item| item["@type"] == "Recipe" }
+    
+      if recipe
+        description = recipe["description"]
+        ingredients = recipe["recipeIngredient"]
+        instructions = recipe["recipeInstructions"].map { |step| step["text"] }
+      else
+        ingredients = []
+        instructions = []
+      end
+
+      raw_recipe = {
+        "title" => title,
+        "author" => author,
+        "image" => image,
+        "description" => description,
+        "ingredients" => ingredients,
+        "instructions" => instructions
+      }
+
+      return raw_recipe
+
+    end
+
+    render json: {raw_recipe: raw_recipe }
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
+
+  end
+
 end
