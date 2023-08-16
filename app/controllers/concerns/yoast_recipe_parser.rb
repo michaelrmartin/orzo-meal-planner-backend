db113f2f50bbb7354dd721b1dd4f3d8f009d6327
michaelrmartin <iammichaelmartin@gmail.com>
2023-08-16 12:42:25 -0500

    updates yoastrecipeparser

--- app/controllers/concerns/yoast_recipe_parser.rb
+++ app/controllers/concerns/yoast_recipe_parser.rb
@@ -9,34 +9,40 @@ module YoastRecipeParser
 
     article = parsed_data["@graph"].find { |item| item["@type"] == "Article" }
 
+    recipe = parsed_data["@graph"].find { |item| item["@type"] == "Recipe" }
+
     if article
       title = article["headline"]
       chef = article["author"]["name"]
-      
-      recipe = parsed_data["@graph"].find { |item| item["@type"] == "Recipe" }
-      
-      if recipe
-        description = recipe["description"]
-        images = recipe["image"]
-        ingredients = recipe["recipeIngredient"]
-        instructions = recipe["recipeInstructions"].map { |step| step["text"] }
-        
-        cleaned_instructions = instructions.map do |step|
-          step.gsub(/[^0-9A-Za-z\p{Punct}\s]/, ' ')
-        end
-        
-        raw_recipe = {
-          "title" => title,
-          "chef" => chef,
-          "images" => images,
-          "description" => description,
-          "ingredients" => ingredients,
-          "instructions" => cleaned_instructions,
-        }
-      end
+    else 
+      title = recipe["name"]
+      chef = recipe["author"]
     end
+
+    description = recipe["description"].gsub(/[^0-9A-Za-z\p{Punct}\s]/, ' ')
+    images = recipe["image"]
+    ingredients = recipe["recipeIngredient"]
+    instructions = recipe["recipeInstructions"].map { |step| step["text"] }
+    
+    cleaned_ingredients = ingredients.map do |ingredient|
+      ingredient.gsub(/[^0-9A-Za-z\p{Punct}\s]/, ' ')
+    end
+    
+    cleaned_instructions = instructions.map do |step|
+      step.gsub(/[^0-9A-Za-z\p{Punct}\s]/, ' ')
+    end
+        
+    raw_recipe = {
+      "title" => title,
+      "chef" => chef,
+      "images" => images,
+      "description" => description,
+      "ingredients" => cleaned_ingredients,
+      "instructions" => cleaned_instructions,
+      }
 
     raw_recipe
 
   end
+
 end
