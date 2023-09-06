b7ffd82933a1c40894fdf7f0edea18f01a4484c4
michaelrmartin <iammichaelmartin@gmail.com>
2023-09-06 12:52:08 -0500

    Adds index action to ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,8 +1,14 @@
 class IngredientsController < ApplicationController
 
   def index 
+    ingredients = Ingredient.all
+
+    render json: ingredients.as_json
     
   end
 
+  def show
+
+  end
 
 end
